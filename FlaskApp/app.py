ecf12e8e7663b42e759e0c25711b3e74b940660b
VINEESH T.P <vineshtp@telxsi.com>
2019-06-25 14:02:21 +0200

    added

---
+++ FlaskApp/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, flash, redirect,render_template, request, session, abort
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Flask App"
+
+@app.route("/hello/<string:name>/")
+def hello(name):
+    return render_template('test.html', name=name) #</string:name>
+
+@app.route("/Members")
+def members():
+    return "Members!"
+
+@app.route("/members/<string:name>/")
+def getMember(name):
+    return name
+
+if __name__ == '__main__':
+    app.run() #(host='0.0.0.0', port=80)
