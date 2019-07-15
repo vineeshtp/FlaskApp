74b0bdf3b16f9e4a2edd20854bcca57625ebb9a7
Vineesh <vineeshtp88@gmail.com>
2019-07-15 21:48:45 +0200

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Flask App updated"
+    return "Flask App updated  updated from git hub"
 
 @app.route("/hello/<string:name>/")
 def hello(name):
