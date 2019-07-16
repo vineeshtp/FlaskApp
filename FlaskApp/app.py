42f7d36def31edd85b28c11c0187abf9c879813d
Vineesh <vineeshtp88@gmail.com>
2019-07-16 10:12:26 +0200

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Flask App - latest -2"
+    return "Flask App - latest -3"
 
 @app.route("/hello/<string:name>/")
 def hello(name):
