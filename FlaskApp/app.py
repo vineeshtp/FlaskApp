801774f9be57dddb64f4bcfb01f26dd38a06fd80
Vineesh <vineeshtp88@gmail.com>
2019-07-16 09:07:30 +0200

    Update app.py
    updated...

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World! port number changed NNNN"
+    return "Hello World! port number changed PPPP"
 
 @app.route("/hello/<string:name>/")
 def hello(name):
