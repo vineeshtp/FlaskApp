d5d9b3854fa384d80648819b0bdec7e9e43ff719
Vineesh <vineeshtp88@gmail.com>
2019-07-17 22:26:40 +0200

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -21,5 +21,5 @@ def getMember(name):
 
 if __name__ == '__main__':
     # app.run() #(host='0.0.0.0', port=80)
-    app.run(host='0.0.0.0', port=8084)
+    app.run(host='127.0.0.1', port=8084,debug=True)
     app.quit()
