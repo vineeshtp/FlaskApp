fba765ebf3c56947693be4a98ee2bbcb232b5aec
VINEESH T.P <vineshtp@telxsi.com>
2019-07-18 15:41:29 +0200

    'ff'

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Flask App .... 2335vv "
+    return "Flask App .... 2335vv  newnw updated."
 
 @app.route("/hello/<string:name>/")
 def hello(name):
